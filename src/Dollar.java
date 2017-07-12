6e376cc81cf13105c8a4f1fc7b24ddfb16822896
lavender21 <1573872488@qq.com>
2017-07-12 15:37:41 +0800

    Init

---
+++ src/Dollar.java
@@ -0,0 +1,10 @@
+public class Dollar {
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiplier){
+        return new Dollar(amount * multiplier);
+    }
+    int amount;
+}
---
+++ test/DollarTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DollarTest {
+    @Test
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10,product.amount);
+        product = five.times(3);
+        assertEquals(15,product.amount);
+    }
+}
