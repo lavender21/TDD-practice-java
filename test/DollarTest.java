7036dd1f092d35e2367a778005ad9c20859f2085
lavender21 <1573872488@qq.com>
2017-07-13 11:39:14 +0800

    苹果和桔子

--- test/DollarTest.java
+++ test/DollarTest.java
@@ -14,6 +14,7 @@ public class DollarTest {
     public void testEquality(){
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertFalse(new Dollar(5).equals(new France(5)));
     }
 
 }
